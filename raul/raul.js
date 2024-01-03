01a2e5f416c71f9c77bdec084a199efa1d125f48
BL-equi <blackzinhazinha@gmail.com>
2024-01-03 15:44:32 -0300

    Add files via upload

---
+++ raul/raul.js
@@ -0,0 +1,9 @@
+function img01 (){
+    document.getElementById("trocarimg").src="White t shirt1.jpg";
+}
+function img02 (){
+    document.getElementById("trocarimg").src="White t shirt2.jpg";
+}
+function img03 (){
+    document.getElementById("trocarimg").src="White t shirt3.jpg";
+}
