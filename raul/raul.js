0369b7942f02d1625bb9f875ad909d2bb746c380
BL-equi <blackzinhazinha@gmail.com>
2024-01-03 15:46:00 -0300

    Delete raul directory

--- raul/raul.js
+++
@@ -1,9 +0,0 @@
-function img01 (){
-    document.getElementById("trocarimg").src="White t shirt1.jpg";
-}
-function img02 (){
-    document.getElementById("trocarimg").src="White t shirt2.jpg";
-}
-function img03 (){
-    document.getElementById("trocarimg").src="White t shirt3.jpg";
-}
